ca3566bfb7a5e57fc1e761015303404e34693fd7
Alex Withington-Smith <alex@awithsmith.com>
2020-11-02 08:54:24 +0000

    tidied up code

--- lib/codetimer.rb
+++ lib/codetimer.rb
@@ -3,13 +3,13 @@ require 'date'
 def run_function(array)
   puts "Starting function with array of size #{array.size}"
   start = DateTime.now.strftime('%Q').to_i
-  array.sort
+  function_to_test(array)
   finish = DateTime.now.strftime('%Q').to_i
   puts "Operation completed in #{finish - start}ms"
 end
 
 def function_to_test(array)
-  sleep(0.1)
+  array.sort
 end
 
 def random_number
@@ -18,17 +18,13 @@ end
 
 def create_array_of_random(size)
   return_array = []
-  size.times { return_array << random_number}
-  return return_array
+  size.times { return_array << random_number }
+  return_array
 end
 
 run_function(create_array_of_random(100))
 run_function(create_array_of_random(1000))
-run_function(create_array_of_random(10000))
-run_function(create_array_of_random(100000))
-run_function(create_array_of_random(1000000))
-run_function(create_array_of_random(10000000))
-
-
-
-
+run_function(create_array_of_random(10_000))
+run_function(create_array_of_random(100_000))
+run_function(create_array_of_random(1_000_000))
+run_function(create_array_of_random(10_000_000))
